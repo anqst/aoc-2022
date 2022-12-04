797d3585bb3c44cdabbd7b7ef7b8b8d355594958
anqst <mocek.ad.2020@skola.ssps.cz>
2022-12-04 10:11:03 +0100

    day 4

---
+++ 4/index1.js
@@ -0,0 +1,29 @@
+const os = require("os");
+const fs = require("fs");
+const input = fs.readFileSync("./input.in", "utf8");
+const pairs = input.trim().split(os.EOL);
+
+let total = 0;
+
+pairs.forEach(function (pair) {
+
+    const pairArr = [];
+    const checkedIndexPair = [];
+
+    pair.split(",").forEach(function (elf) {
+        pairArr.push([parseInt(elf.split("-")[0]), parseInt(elf.split("-")[1])]);
+    });
+
+    for (let i = 0; i < pairArr.length; i++) {
+
+        for (let y = 0; y < pairArr.length; y++) {
+
+            if (pairArr[i][0] >= pairArr[y][0] && pairArr[i][1] <= pairArr[y][1] && i !== y && checkedIndexPair.indexOf(`${Math.min(i, y)}-${Math.max(i, y)}`) === -1) {
+                total += 1;
+                checkedIndexPair.push(`${Math.min(i, y)}-${Math.max(i, y)}`)
+            }
+        }
+    }
+});
+
+fs.writeFileSync("./test1.out", total.toString(), "utf8");
