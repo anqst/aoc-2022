14c2d2c7ace23d483d6f6dd438d2a7eb76505b7f
anqst <mocek.ad.2020@skola.ssps.cz>
2022-12-08 16:57:04 +0100

    day 6

---
+++ 6/index1.js
@@ -0,0 +1,24 @@
+const os = require("os");
+const fs = require("fs");
+const input = fs.readFileSync("./input.in", "utf8").trim();
+const charByChar = input.split("");
+
+const mustBeUniqueCount = 4;
+let uniquesCount = 0;
+
+for (let i = 0; i < charByChar.length; i++) {
+
+    const char = charByChar[i];
+    const nextIndex = charByChar.indexOf(char, i + 1);
+
+    if (nextIndex === -1 || Math.abs(nextIndex - i) >= mustBeUniqueCount - uniquesCount) {
+        uniquesCount++;
+    } else {
+        uniquesCount = 0;
+    }
+
+    if (uniquesCount >= mustBeUniqueCount) {
+        fs.writeFileSync("./test1.out", (i + 1).toString(), "utf8");
+        break;
+    }
+}
